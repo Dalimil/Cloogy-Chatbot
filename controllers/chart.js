867cb92f1ac56d986675bafe0fa3aee8244ed64e
Thomas Obermüller <thomas.obermueller@gmail.com>
2016-06-04 07:54:07 +0100

    Formatting

--- controllers/chart.js
+++ controllers/chart.js
@@ -8,19 +8,18 @@ exports.chart = function(cb){ // route
     return date.getHours() + ':' + date.getMinutes();
   });
   var yLabels = raw.map(function(x) { return (x.Read * 1000) + ' W'; });
-  // var width = 250;
-  // var height = 100;
-  const imgSize = 800;
+    const imgSize = 200;
     var width = 1.91 * imgSize;
     var height = 1 * imgSize;
 
   var args = [
-    'cht=lc',
-    'chxt=x,y',
+    'cht=lc:nda',
+    // 'chxt=x,y',
     'chd=t:' + data.join(','),
     'chs=' + width + 'x' + height,
     'chxl=0:|' + xLabels.join('|') + '|1:|' + yLabels.join('|'),
-    'chco=00BFB3'
+    'chco=00BFB3',
+    'chma=30,30,30,30'
   ];
 
   var uri = 'https://chart.googleapis.com/chart?' + args.join('&');
